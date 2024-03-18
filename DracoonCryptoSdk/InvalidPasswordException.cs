a01c594da1e973d7ef022619d251a45c599b6af4
Sebastian Hübner <25882725+shuebner20@users.noreply.github.com>
2024-03-18 01:13:54 +0100

    add support for .NET 8

--- DracoonCryptoSdk/InvalidPasswordException.cs
+++ DracoonCryptoSdk/InvalidPasswordException.cs
@@ -18,6 +18,10 @@ namespace Dracoon.Crypto.Sdk {
         public InvalidPasswordException(string message, Exception cause) : base(message, cause) { }
 
         /// <inheritdoc/>
+#if NET8_0_OR_GREATER
+        // Serialization ctor is obsolete starting with .NET 8.0
+        [Obsolete("Legacy serialization infrastructure APIs marked obsolete (https://github.com/dotnet/docs/issues/34893)", DiagnosticId = "SYSLIB0051")]
+#endif
         protected InvalidPasswordException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
